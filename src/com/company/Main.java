c9a0d9e34de811cfec41f1de88fe5972ed90c3d8
Vlad Pr <priselkov@tm10.ru>
2019-06-28 08:06:14 +0500

    problem 7 solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         long startTime = System.currentTimeMillis();
-        //System.out.println(new Task3(600851475143L).getAnswer());
-        System.out.println(new Task4().getAnswer());
+        System.out.println(new Task7().getAnswer());
         System.out.println("It takes: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
--- src/com/company/Task7.java
+++ src/com/company/Task7.java
@@ -5,32 +5,26 @@ import java.util.Arrays;
 public class Task7 {
     private int counter = 0;
 
-    private boolean[] firstArr = new boolean[Integer.MAX_VALUE/30000];
+    private boolean[] firstArr = new boolean[Integer.MAX_VALUE / 3000];
 
-    public long getAnswer(){
+    // Немного модифицированное решето Эратосфена
+    public long getAnswer() {
         Arrays.fill(firstArr, true);
-        for (int i = 0; i<firstArr.length; i++) {
-            if (firstArr[i]){
-                int idx = 0;
-                int chislo = (2 * (i + 1) + 1);
-                for (int j = chislo; idx<firstArr.length && Integer.MAX_VALUE / j < i; j++) {
-//                    int chislo = (2 * (i + 1) + 1) * j;
-                    if (chislo * j % 2 == 1) {
-                        idx = (((chislo*j)-1)/2)-1;
-                        if(idx<firstArr.length && idx>0 && Integer.MAX_VALUE / j < i) firstArr[idx] = false;
-                        if(!firstArr[2]) {
-                            System.out.println(chislo);
-//                            System.out.println(j);
-                            System.out.println(/*141201*152087 < */Integer.MAX_VALUE / 152087);
-                        }
-                    }
+        for (int i = 0; i < firstArr.length; i++) {
+            if (firstArr[i]) {
+                // В индексе массива закодировано число
+                long chislo = (2 * (i + 1) + 1);
+                for (long j = chislo; (((chislo * j) - 1) / 2) - 1 < firstArr.length; j++) {
+                    int idx = (int) ((((chislo * j) - 1) / 2) - 1);
+                    // Четные числа не участвуют
+                    if (chislo * j % 2 == 1) firstArr[idx] = false;
                 }
             }
         }
-        for(int i = 0; i<firstArr.length; i++){
+        for (int i = 0; i < firstArr.length; i++) {
             if (firstArr[i]) counter++;
-            if (counter==10000) return 2*(i+1)+1;
+            if (counter == 10000) return 2 * (i + 1) + 1;
         }
-        return 0;
+        return -1;
     }
 }
