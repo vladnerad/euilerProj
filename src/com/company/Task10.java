5da9ef452c1e43ffcb0bbb7f1bf29a9ab763b1ec
Vladislav Prisel <thief37@mail.ru>
2019-06-30 18:37:28 +0500

    problem #10 solved

--- src/com/company/Task10.java
+++ src/com/company/Task10.java
@@ -2,10 +2,10 @@ package com.company;
 
 import java.util.Arrays;
 
-public class Task7 {
-    private int counter = 0;
+public class Task10 {
 
-    private boolean[] firstArr = new boolean[Integer.MAX_VALUE / 3000];
+    private boolean[] firstArr = new boolean[Integer.MAX_VALUE / 2000];
+    private long result = 0;
 
     // Немного модифицированное решето Эратосфена
     public long getAnswer() {
@@ -13,26 +13,18 @@ public class Task7 {
         for (int i = 0; i < firstArr.length; i++) {
             if (firstArr[i]) {
                 // В индексе массива закодировано число
-                long chislo = getNumberFromIdx(i);
+                long chislo = Task7.getNumberFromIdx(i);
                 for (long j = chislo; (((chislo * j) - 1) / 2) - 1 < firstArr.length; j++) {
-                    int idx = getIdxFromNumber(chislo, j);
+                    int idx = Task7.getIdxFromNumber(chislo, j);
                     // Четные числа не участвуют
                     if (chislo * j % 2 == 1) firstArr[idx] = false;
                 }
             }
         }
         for (int i = 0; i < firstArr.length; i++) {
-            if (firstArr[i]) counter++;
-            if (counter == 10000) return 2 * (i + 1) + 1;
+            long buf = Task7.getNumberFromIdx(i);
+            if (firstArr[i] && buf < 2000000) result += buf;
         }
-        return -1;
-    }
-
-    public static long getNumberFromIdx (int idx){
-        return (2 * (idx + 1) + 1);
-    }
-
-    public static int getIdxFromNumber (long prime, long factor){
-        return (int) ((((prime * factor) - 1) / 2) - 1);
+        return result + 2; //т.к. в массиве счет начинается с трех
     }
 }
