fc529602d9302cff607fae33b0511d45a8669dba
Vlad Pr <priselkov@tm10.ru>
2019-07-04 07:06:28 +0500

    problem 12 solved

--- src/com/company/Task12.java
+++ src/com/company/Task12.java
@@ -2,48 +2,61 @@ package com.company;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
-public class Task12 implements EulerTask{
+public class Task12 implements EulerTask {
 
     private int result = 0;
-    private boolean[] firstArr = new boolean[Integer.MAX_VALUE / 5000];
+    private boolean[] firstArr = new boolean[100];
     private HashMap<Integer, Integer> kanonRazlozh;
+    long buff = 0;
+    private int factorsQuantity = 500;
 
     @Override
     public int getAnswer() {
-//        for (int i = 1; i<Integer.MAX_VALUE;i++){
-//            int factorCount = 0;
-//            result+=i;
-//            for (int j = 1; j <= result; j++){
-//                if (result%j==0){
-//                    factorCount++;
-//                }
-//                if (factorCount>500) return i;
-//            }
-//        }
-
         sieveErFill(firstArr);
-        for (int i = 1; i<Integer.MAX_VALUE;i++) {
-            int idx = 0;
+        for (int i = 1; i < Integer.MAX_VALUE / 2; i++) {
+            //int idx = 0;
+            //Карта с каноноческий разложение, ключ - числа, значение = степень
             kanonRazlozh = new HashMap<>();
-            int factorCount = 0;
+            //Количество делителей
+            int factorCount = 1;
+            //Рассматриваемое треугольное число
             result += i;
-            int factor = 2;
-            int factQuan = 0;
-            for (int j = 2; j<Integer.MAX_VALUE; ) {
-                while (result % factor == 0) {
-                    factQuan++;
-                    if (!kanonRazlozh.replace(factor, factQuan - 1, factQuan)) kanonRazlozh.put(factor, 1);
+            //Начинаем проверку с делителя 2
+            int factor = 1;
+            int zal = result;
+            do {
+                //Степень делителя
+                int factPow = 0;
+                factor++;
+                while (zal % factor == 0) {
+                    factPow++;
+                    if (!kanonRazlozh.containsKey(factor)) kanonRazlozh.put(factor, 1);
+                    else kanonRazlozh.replace(factor, factPow);
+                    zal /= factor;
+                    if (zal == 1) break;
                 }
-                if (j)
+            } while (zal != 1);
+//                factorCount++;
+            for (int z = 0; z < firstArr.length; z++) {
+                buff = Task7.getNumberFromIdx(z);
+                if (firstArr[z] && buff > factor) {
+                    break;
+                }
+                if (factor > result) break;
+            }
+            for (Map.Entry<Integer, Integer> entry : kanonRazlozh.entrySet()) {
+                factorCount *= (entry.getValue() + 1);
+            }
+            if (factorCount > factorsQuantity) {
+                return result;
             }
         }
-
-
         return -1;
     }
 
-    public void sieveErFill (boolean[] firstArr){
+    public void sieveErFill(boolean[] firstArr) {
         Arrays.fill(firstArr, true);
         for (int i = 0; i < firstArr.length; i++) {
             if (firstArr[i]) {
