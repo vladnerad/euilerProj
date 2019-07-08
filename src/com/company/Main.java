ad38c0379d24d4b0eac0b185da758cad9e202dfe
Vlad Pr <priselkov@tm10.ru>
2019-07-08 07:40:24 +0500

    problem 14 solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         long startTime = System.currentTimeMillis();
-        System.out.println(new Task13().getAnswer());
+        System.out.println(new Task14().getAnswer());
         System.out.println("It takes: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
