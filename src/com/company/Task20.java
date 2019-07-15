e04cf6131a90bf7b657c24cfca3901f87a00c110
Vlad Pr <priselkov@tm10.ru>
2019-07-15 06:52:34 +0500

    problem 20 solved

---
+++ src/com/company/Task20.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.math.BigInteger;
+
+public class Task20 implements EulerTask {
+
+    @Override
+    public int getAnswer() {
+        BigInteger bigInteger = new BigInteger("100");
+        for (int i = 99; i > 1; i--) {
+            bigInteger = bigInteger.multiply(new BigInteger(String.valueOf(i)));
+        }
+        String factorial = bigInteger.toString();
+        int result = 0;
+        for (String x : factorial.split("")) {
+            result += Integer.parseInt(x);
+        }
+        return result;
+    }
+}
