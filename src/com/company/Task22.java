745b929ef5b654be149a79be5d8ed32528740eaf
Vlad Pr <priselkov@tm10.ru>
2019-07-16 07:24:18 +0500

    problem 22 solved

--- src/com/company/Task22.java
+++ src/com/company/Task22.java
@@ -7,35 +7,37 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Task22 {
-    private File namesFile = new File("/home/vladik/java/javaProj/sources/euilerProj/src/com/company/inputs/names.txt");
-    private String fileName = "/home/vladik/java/javaProj/sources/euilerProj/src/com/company/inputs/names.txt";
+    //    private File namesFile = new File("/home/vladik/java/javaProj/sources/euilerProj/src/com/company/inputs/names.txt");
+    //    private String fileName = "/home/vladik/java/javaProj/sources/euilerProj/src/com/company/inputs/names.txt";
+    private String fileNameW = "C:\\Users\\vpriselkov\\IdeaProjects\\euilerProj\\src\\com\\company\\inputs\\names.txt";
 
-    private String[] getNamesFromFile(String fileName){
+    private String[] getNamesFromFile(String fileName) {
 
         try {
-//            String[] array = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).map(s -> s.split(",")).sorted().collect();
-//            String[] array = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8).toArray(String[]::new);
-            String[] array =  Arrays.stream(Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8).toArray(String[]::new)).map(s -> s.split(",")).collect(String[]::new);
-            System.out.println(array.length);
+            String nameStr = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8).get(0);
+            String[] names = nameStr.split(",");
+            names = Arrays.stream(names).map(s -> s.replace("\"", "")).sorted().toArray(String[]::new);
+            return names;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
-
-//        String[] result;
-//        try(FileReader fr = new FileReader(namesFile)){
-//
-//            return result;
-//        } catch (Exception e){
-//            e.printStackTrace();
-//            return null;
-//        }
     }
 
-    public int getAnswer(){
-        getNamesFromFile(fileName);
-        return 0;
+    public long getAnswer() {
+        String[] names = getNamesFromFile(fileNameW);
+        long result = 0;
+        for (int i = 0; i < names.length; i++) {
+            int nameScore = 0;
+            for (char c : names[i].toCharArray()) {
+                nameScore += (c - 64);
+            }
+            result += nameScore * (i + 1);
+        }
+        return result;
     }
 }
