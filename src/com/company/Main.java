4dddb8ede5edd8c96bb12282e2ac3ebd42c1a781
Vladislav Prisel <thief37@mail.ru>
2019-07-19 20:35:47 +0500

    problem #25 solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         long startTime = System.currentTimeMillis();
-        System.out.println(new Task24().getAnswer());
+        System.out.println(new Task25().getAnswer2());
         System.out.println("It takes: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
---
+++ src/com/company/Task25.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.math.BigInteger;
+
+public class Task25 implements EulerTask {
+
+    private BigInteger current = new BigInteger("3");
+    private BigInteger previous = new BigInteger("2");
+    private int count = 4;
+
+    //Brute force(
+    @Override
+    public int getAnswer() {
+        while (current.toString().length() != 1000) {
+            BigInteger tmp = previous;
+            previous = current;
+            current = previous.add(tmp);
+            count++;
+        }
+        return count;
+    }
+
+    //Solution from comments
+    //Binet's Fibonacci Number Formula
+
+    //golden ratio
+    static final double g = (Math.sqrt(5) + 1) / 2;
+
+    public int getAnswer2() {
+        int i = 1;
+        double temp = 1;
+        while (temp < 999) {
+            i++;
+            temp = i * Math.log10(g) + Math.log10(1 / Math.sqrt(5));
+        }
+        return i;
+    }
+}
