747587d20d7a57ad45fb7624dec401440543790a
Vlad Pr <priselkov@tm10.ru>
2020-01-14 08:40:35 +0500

    problem 53 solved, need review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,14 @@
 package com.company;
 
-import com.company.fifthten.Task50;
+import com.company.sixteen.Task53;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         long startTime = System.currentTimeMillis();
-        System.out.println(new Task50().getAnswer());
+//        System.out.println(com.company.sixteen.Task51Kt.isPrime(20));
+        System.out.println(new Task53().getAnswer());
         System.out.println("It takes: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
---
+++ src/com/company/sixteen/Task51.java
@@ -0,0 +1,24 @@
+package com.company.sixteen;
+
+import com.company.EulerTask;
+
+public class Task51 implements EulerTask {
+
+    @Override
+    public int getAnswer() {
+        for (int i = 56997; i < Integer.MAX_VALUE; i+=2){
+            String numStr = String.valueOf(i);
+
+        }
+        return 0;
+    }
+
+    private boolean isPrime(long n) {
+        if (n % 2 == 0) return n == 2;
+        if (n == 1) return false;
+        for (int i = 3; i * i < n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
